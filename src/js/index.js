f1bd47e389902e2451a43f4da4637016eda0da94
anandrikka <appsbyanand@gmail.com>
2021-05-02 01:28:36 -0400

    upgraded css much cleaner and easier to maintain projectn

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,85 @@
 // CSS Loading....
-require('../../node_modules/bootstrap/dist/css/bootstrap-reboot.css');
-require('../../node_modules/bootstrap/dist/css/bootstrap-grid.css');
-require('../css/main.css');
-require('../css/nav.css');
+require('../../node_modules/bootstrap/dist/css/bootstrap.min.css');
+require('../css/index.css');
+require('../css/timeline.css');
 
 // Javascript Loading....
+require('../../node_modules/bootstrap/js/dist/collapse');
 window.$ = require('jquery');
-const Masonry = require('masonry-layout');
 const Typed = require('typed.js');
+const Circles = require('./circle');
 
-// Preloader
-(function loader() {
-  $('#preloader').delay(500).fadeOut('slow')
-}());
+const selectors = {
+  sun: '#float-theme-icon .fa-sun',
+  moon: '#float-theme-icon .fa-moon',
+  themeIconContainer: '#float-theme-icon',
+  typed: '#role-typed',
+  navItem: '#navbarToggler ul li',
+  mainSection: '.main-section',
+  softSkills: '.soft-skills',
+  projectButton: '#projects .projects > button'
+}
 
-$(function () {
-  // Masonry
-  new Masonry(document.querySelector('.masonary-grid'), {
-    itemSelector: '.masonary-grid-item'
+const colors = [
+  ['#d32f2f', '#ff5252'],
+  ['#c2185b', '#ff4081'],
+  ['#7b1fa2', '#7b1fa2'],
+  ['#512da8', '#7c4dff'],
+  ['#00796b', '#64ffda'],
+  ['#00838f', '#18ffff']
+];
+
+const skillCircles = [];
+
+function setClassesForTheme(theme) {
+  if (theme === 'theme-dark') {
+    $(selectors.sun).removeClass('hide');
+    $(selectors.moon).addClass('hide');
+  } else {
+    $(selectors.sun).addClass('hide');
+    $(selectors.moon).removeClass('hide');
+  }
+}
+
+function toggleThemeListener() {
+  const theme = document.documentElement.className === 'theme-dark' ? 'theme-light' : 'theme-dark';
+  document.documentElement.className = theme;
+  localStorage.theme = theme;
+  setClassesForTheme(theme);
+}
+
+function toggleSection() {
+  $(selectors.navItem).removeClass('active');
+  $(this).addClass('active');
+  const dataId = $(this).attr('data-id');
+  $(selectors.mainSection).removeClass('active');
+  $(`.main-section#${dataId}`).addClass('active');  
+  $('#navbarToggler').removeClass('show');
+  skillCircles.forEach(c => {
+    dataId === 'aboutme' ? c.update(100) : c.update(0)
   })
+}
+
+// Initialization.
+$(function () {
+  // preloader
+  $('#preloader').delay(500).fadeOut('slow')
+
+  // Theme setter
+  document.documentElement.className = localStorage.theme || 'theme-light';
+  localStorage.theme = document.documentElement.className;
+  $(selectors.themeIconContainer).on('click', toggleThemeListener);
+  setClassesForTheme(localStorage.theme);
+
+  // Nav item selector
+  $(selectors.navItem).on('click', toggleSection);
+
   // Typed
   new Typed('#role-typed', {
-    strings: ["Web Development"],
+    strings: ["A Web developer...", "I do React, Angular, Java...", "I love to work on fun projects..."],
     stringsElement: null,
-    typeSpeed: 70,
-    startDelay: 150,
+    typeSpeed: 80,
+    startDelay: 100,
     backSpeed: 40,
     backDelay: 350,
     loop: true,
@@ -33,63 +88,32 @@ $(function () {
     cursorChar: "|",
     attr: null,
     contentType: "html",
-  })
-})
-// Clicking on bars icon to oopen menu
-$(".menu-toggle .fa-bars").on("click", function (e) {
-  $(".nav-container").css('display', 'block');
-  $(this).removeClass('show');
-  $(this).removeClass('hide');
-  $(this).addClass('hide');
-  $(".menu-toggle .fa-times").removeClass('hide');
-  $(".menu-toggle .fa-times").removeClass('show');
-  $(".menu-toggle .fa-times").addClass('show');
-});
+  });
 
-// Click on close button to close the menu
-$(".menu-toggle .fa-times").on("click", function (e) {
-  $(".nav-container").css('display', 'none');
-  $(this).removeClass('show');
-  $(this).removeClass('hide');
-  $(this).addClass('hide');
-  $(".menu-toggle .fa-bars").removeClass('hide');
-  $(".menu-toggle .fa-bars").removeClass('show');
-  $(".menu-toggle .fa-bars").addClass('show');
-})
+  // Soft skills
+  $(selectors.softSkills).each(function (index) {
+    skillCircles.push(Circles.create({
+      id: $(this).attr('id'),
+      radius: 85,
+      value: 0,
+      maxValue: 100,
+      width: 12,
+      text: $(this).attr('data-text'),
+      colors: document.documentElement.className === 'theme-dark' ? ['#424242', colors[index][0]] : ['#C0C0C0', colors[index][1]],
+      duration: 2000,
+      wrpClass: 'progress-circle-wrap',
+      textClass: 'progress-circle-text',
+      valueStrokeClass: 'circles-valueStroke',
+      maxValueStrokeClass: 'circles-maxValueStroke'
+    }));
+  });
 
-// reset the styles and classes once the window resized to > 578.
-$(window).on('resize', function () {
-  const win = $(this);
-  if (win.width() > 578) {
-    $(".nav-container").css('display', '');
-    $(".menu-toggle .fa-times").removeClass('show');
-    $(".menu-toggle .fa-times").removeClass('hide');
-    $(".menu-toggle .fa-times").addClass('hide');
-    $(".menu-toggle .fa-bars").removeClass('hide');
-    $(".menu-toggle .fa-bars").removeClass('show');
-    $(".menu-toggle .fa-bars").addClass('show');
-  }
-})
-
-// navbar
-$(".navbar .navbar-item").on("click", function (e) {
-  if (!$(this).hasClass("active")) {
-    var tabNum = $(this).index();
-    var nthChild = tabNum + 2;
-    $(".navbar .navbar-item").removeClass("active");
-    $(this).addClass("active");
-    $("#main > section.active").removeClass("active");
-    $(`#main > section:nth-child(${nthChild})`).addClass("active");
-  }
-  // close the nav, when clicked on item
-  if (window.innerWidth < 578) {
-    $(".nav-container").css('display', 'none');
-    $(".menu-toggle .fa-times").removeClass('show');
-    $(".menu-toggle .fa-times").removeClass('hide');
-    $(".menu-toggle .fa-times").addClass('hide');
-    $(".menu-toggle .fa-bars").removeClass('hide');
-    $(".menu-toggle .fa-bars").removeClass('show');
-    $(".menu-toggle .fa-bars").addClass('show');
-  }
-  e.preventDefault();
+  // Projects
+  $(selectors.projectButton).on('click', function () {
+    $('#projects .btn-group.projects button').removeClass('active');
+    $('#projects .project-item').removeClass('active');
+    const tabId = $(this).attr('data-id')
+    $(this).addClass('active');
+    $(`#${tabId}`).addClass('active')
+  });
 });
